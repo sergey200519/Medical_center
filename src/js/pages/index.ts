039482c620b8607d8f9e38a482265447cf2c9eba
sergey <oborok05@bk.ru>
2024-07-02 00:36:42 +0300

    deleted the excess

--- src/js/pages/index.ts
+++ src/js/pages/index.ts
@@ -17,39 +17,3 @@ newBannerSwiper(document.querySelector(".swiper-section_swiper"), {
 });
 
 
-// TODO: not useed
-// newTeamSwiper(document.querySelector(".team_swiper"), {
-//     "parent": document.querySelector(".team"),
-//     "slideClass": "swiper_body_slide",
-//     "pagination": false,
-//     "swipe": {
-//         "swipe": "tranlate"
-//     },
-//     "autoPlay": false,
-//     "nSlideSimultaneously": 5,
-//     "arrow": {
-//         "leftArrowClass": "swiper_prev",
-//         "rightArrowClass": "swiper_next"
-//     },
-//     "more": {
-//         "varPadding": "--team_swiper-padding",
-//         "varGap": "--team_swiper-gap",
-//         "varWidth": "--team_slide-width"
-
-//     }
-// });
-
-// const swiper = new Swiper('.sample-slider', {
-//     loop: true, 
-//     pagination: {                       // pagination(dots)
-//         el: '.swiper-pagination',
-//     },
-//     navigation: {                       // navigation(arrows)
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev",
-//     },
-// })
-
-
-// const swiper2 = new SwiperBase("team_swiper", {})
-// console.log(swiper2);
