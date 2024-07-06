a1fa5b27ce2211308a9dede67720af0e9c416f77
sergey <oborok05@bk.ru>
2024-07-06 20:46:26 +0300

    fix index.html(menu, aside, banner, servises)

--- src/js/modules/aside.ts
+++ src/js/modules/aside.ts
@@ -14,21 +14,28 @@ class AsideMenu {
         e.preventDefault();
         const element: HTMLElement = e.target as HTMLElement;
         const menuItem: HTMLElement = element.closest("li");
-        const submenu: HTMLElement =  menuItem.querySelector("menu");
+        const submenu: HTMLElement = menuItem.querySelector("menu");
+        const submenuItems: NodeListOf<HTMLElement> = submenu.querySelectorAll(":scope > li");
         if (this.oldMenuItems == menuItem) {
             return;
         }
-        // this.hide();
-        
+
         submenu.classList.remove("none");
         menuItem.classList.add("active");
         const menuItemHeight: number = menuItem.getBoundingClientRect().height;
-        submenu.style.height = `${menuItemHeight * submenu.querySelectorAll("li").length}px`;
+        submenu.style.height = `${menuItemHeight * submenuItems.length}px`;
+        let newHeight: number = 0;
+        submenuItems.forEach((item: HTMLElement) => {
+            newHeight += item.getBoundingClientRect().height;
+        })
+        if (newHeight > (menuItemHeight * submenuItems.length)) {
+            submenu.style.height = `${newHeight}px`;
+        }
         this.oldMenuItems = menuItem;
     }
 
     hide() {
-        [...this.menuItems].forEach((element: HTMLElement):  void => {
+        [...this.menuItems].forEach((element: HTMLElement): void => {
             const submenu: HTMLElement = element.querySelector(".submenu");
             element.classList.remove("active");
             submenu.classList.add("none");
