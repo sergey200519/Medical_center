87bfc034f13f37b9e3714ed5d089386d1aa27ec3
sergey <oborok05@bk.ru>
2024-09-28 12:40:18 +0300

    Update 2024-09-28T09:39:23.116Z

---
+++ js/pages/pricelist_laboratory/pricelist_laboratory.js
@@ -0,0 +1,100 @@
+const url = "https://sergey200519-medical-center-backend-flask-06b4.twc1.net/services/laboratory";
+const pricelistBox = document.querySelector(".det");
+let pricelistCode = "";
+function render(services) {
+    for (const [key, value] of Object.entries(services)) {
+        let tableCode = `
+            <thead>
+                <tr>
+                    <th id="keys">Код</th>
+                    <th>Название услуги</th>
+                    <th>Биоматериал</th>
+                    <th>Срок</th>
+                    <th>Цена(в руб.)</th>
+                </tr>
+            </thead>
+        `;
+        console.log(key, value);
+        let tbodyCode = ``;
+        for (const [k, v] of Object.entries(value)) {
+            tbodyCode += `
+                <tr>
+                    <th scope="rowgroup" headers="keys" colspan="5" class="sub-title">${k}</th>
+                </tr>
+            `;
+            for (const [k2, v2] of Object.entries(v)) {
+                tbodyCode += `
+                    <tr>
+                        <td>${k2}</td>
+                        <th scope="row">${v2.name}</th>
+                        <td>${v2.bio}</td>
+                        <td>${v2.date}</td>
+                        <td>${v2.price}</td>
+                    </tr>
+                `;
+            }
+        }
+        // for (const [k, v] of Object.entries(value)) {
+        //     if (Object.keys(v).length == 1) {
+        //         tbodyCode += `
+        //             <tr>
+        //                 <td>${k}</td>
+        //                 <th scope="row">${v[Object.keys(v)[0] as unknown as number].service}</th>
+        //                 <td>${v[Object.keys(v)[0] as unknown as number].price}</td>
+        //             </tr>
+        //         `;
+        //     } else {
+        //         let isFirst: boolean = true;
+        //         for (const [n, s] of Object.entries(v)) {
+        //             // console.log(n, s);
+        //             if (isFirst) {
+        //                 tbodyCode += `
+        //                 <tr>
+        //                     <td rowspan="${Object.keys(v).length}">${k}</td>
+        //                     <th scope="row">${s.service}</th>
+        //                     <td>${s.price}</td>
+        //                 </tr>
+        //             `
+        //             isFirst = false
+        //             } else {
+        //                 tbodyCode += `
+        //                     <tr>
+        //                         <th scope="row">${s.service}</th>
+        //                         <td>${s.price}</td>
+        //                     </tr>
+        //                 `;
+        //             }
+        //         }
+        //     }
+        // }
+        tableCode += tbodyCode;
+        pricelistCode += `
+            <details>
+                <summary>${key}</summary>
+                <table class="pricelist-table">
+                    ${tableCode}
+                </table>
+            </details>
+        `;
+    }
+    pricelistBox.innerHTML = pricelistCode;
+}
+fetch(url, {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+})
+    .then(response => {
+    if (!response.ok) {
+        throw new Error('Ошибка запроса');
+    }
+    return response.json();
+})
+    .then(data => {
+    console.log(data);
+    render(data);
+})
+    .catch(error => {
+    console.log(error);
+});
---
+++ js/pages/specialists/specialists.js
@@ -0,0 +1,29 @@
+import { newAppointmentModal } from "../../modules/appointmentModal.min.js";
+document.addEventListener("DOMContentLoaded", () => {
+    const appointmentModal = document.querySelector(".appointment_modal");
+    [...document.querySelectorAll(".specialists .appointment_btn-specialist")].forEach((btn) => {
+        const specialistName = btn.getAttribute("data-specialist");
+        newAppointmentModal(appointmentModal, {
+            "btnOpen": btn,
+            "btnClose": appointmentModal.querySelector(".appointment_modal_body_clouse"),
+            "classHide": "none",
+            "more": {
+                "bodyOpenClass": "open",
+                "specialistName": specialistName
+            }
+        });
+    });
+    [...document.querySelectorAll(".specialists_list_item")].forEach((specialistCard) => {
+        specialistCard.addEventListener("click", (e) => {
+            e.preventDefault();
+            window.location.href = `${window.location.href.replace("/specialists.html", "")}/components/pages/${specialistCard.getAttribute("data-page")}.html`;
+        });
+    });
+    // setTimeout(() => {
+    //     window.location.href = `${window.location.href.replace("/specialists.html", "")}/components/pages/makova.html`
+    // }, 3000);
+    // newServices(document.querySelector(".services_wrap"), {
+    //     "classActivator": "active",
+    //     "tegAccordionItems": "li"
+    // });
+});
